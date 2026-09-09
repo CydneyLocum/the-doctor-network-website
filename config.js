9359ebda64a2e079e4b8d8d913a478b837def0dc
CydneyLocum <cydneyhughes@hotmail.com>
2026-09-09 14:08:02 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// The Doctor Network V5 integration config
-// After you deploy the included Google Apps Script as a Web App,
-// paste the /exec URL between the quotes below.
-window.TDN_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx6bVdfCB4bzHsafedCs0e3LaM66VW3jIccmgGiJaXSD6yRDIzmVei_-C7hFUS8Y99swg/exec",
-  FORM_RECIPIENT_DISPLAY: "cydney@thedoctornetwork.com.au"
-};
